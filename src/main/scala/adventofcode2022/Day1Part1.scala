100a11b458e008536fd03f4a76dd6209a54f3e9f
Petr Kubelka <petr.kubelka@ematiq.com>
2022-12-01 21:41:02 +0100

    day1

---
+++ src/main/scala/adventofcode2022/Day1Part1.scala
@@ -0,0 +1,31 @@
+package adventofcode2022
+
+object Day1Part1 {
+
+  def parseInput(input: String) =
+    input
+      .split("\n\n")
+      .map(_.split("\n"))
+      .map(_.map(_.toInt))
+
+  def solve1(parsedInput: Array[Array[Int]]): Int =
+    parsedInput.map(_.sum).max
+
+  def solve2(parsedInput: Array[Array[Int]]): Int =
+    parsedInput
+      .map(_.sum)
+      .sorted
+      .reverse
+      .take(3)
+      .sum
+
+  lazy val input: String = io.Source
+    .fromInputStream(getClass.getResourceAsStream("day1.txt"))
+    .mkString
+    .trim
+
+  def main(args: Array[String]): Unit = {
+    println(solve1(parseInput(input)))
+    println(solve2(parseInput(input)))
+  }
+}
