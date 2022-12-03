f2d80a5f29fa18dc147e93562f903c3972e1c549
Petr Kubelka <petr.kubelka@ematiq.com>
2022-12-03 16:59:30 +0100

    small fix

--- src/main/scala/adventofcode2022/Day3.scala
+++ src/main/scala/adventofcode2022/Day3.scala
@@ -7,33 +7,33 @@ object Day3 {
       .split("\n")
       .toSeq
 
-  def charToIntSum(chars: Seq[Char]) =
-    chars.map {
+  def charToInt(char: Char) =
+    char match {
       case x if x.isUpper => x.toInt - 38
       case x if x.isLower => x.toInt - 96
-    }.sum
+    }
 
   def solve1(parsedInput: Seq[String]) =
-    charToIntSum(
-      parsedInput
-        .map(x =>
-          (
-            x.slice(0, x.length / 2).toSet,
-            x.slice(x.length / 2, x.length).toSet
-          )
+    parsedInput
+      .map(x =>
+        (
+          x.slice(0, x.length / 2).toSet,
+          x.slice(x.length / 2, x.length).toSet
         )
-        .flatMap(x => x._1.intersect(x._2).toSeq)
-    )
+      )
+      .flatMap(x => x._1.intersect(x._2).toSeq)
+      .map(charToInt)
+      .sum
 
   def solve2(parsedInput: Seq[String]) =
-    charToIntSum(
-      parsedInput
-        .grouped(3)
-        .flatMap { case Seq(a, b, c) =>
-          a.toSet.intersect(b.toSet).intersect(c.toSet)
-        }
-        .toSeq
-    )
+    parsedInput
+      .grouped(3)
+      .flatMap { case Seq(a, b, c) =>
+        a.toSet.intersect(b.toSet).intersect(c.toSet)
+      }
+      .map(charToInt)
+      .sum
+
   lazy val input: String = io.Source
     .fromInputStream(getClass.getResourceAsStream("day3.txt"))
     .mkString
