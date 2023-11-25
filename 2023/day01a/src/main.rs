ba7fb197390e45d2ed96ab7739cc745835b44d20
Petr Kubelka <ksonny4@gmail.com>
2023-11-26 00:33:35 +0100

    files

--- 2023/day01a/src/main.rs
+++ 2023/day01a/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let res = include_str!("input.txt")
+        .lines()
+    println!("{res:?}");
+
+    Ok(())
 }
