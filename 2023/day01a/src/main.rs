d9e90cdcdd7f3f1499f3029d792ad0d290299e5f
Petr Kubelka <ksonny4@gmail.com>
2023-12-01 14:54:04 +0100

    day1a

--- 2023/day01a/src/main.rs
+++ 2023/day01a/src/main.rs
@@ -1,11 +1,31 @@
-use itertools::Itertools;
-
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-
-    let res = include_str!("input.txt")
+fn main() {
+    let res: Vec<String> = include_str!("input.txt")
         .lines()
-    println!("{res:?}");
+        .map(|line| {
+            line.chars()
+                .filter(|ch| ch.is_digit(10))
+                .collect::<String>()
+        })
+        .map(|line: String| {
+            let first_char = line.chars().next();
+            let last_char = line.chars().last();
 
-    Ok(())
+            match (first_char, last_char) {
+                (Some(first), Some(last)) => {
+                    let result = format!("{}{}", first, last);
+                    result
+                }
+                _ => String::from("0"),
+            }
+        })
+        .collect();
+
+    let result: i32 = res.into_iter().map(|s| s.parse::<i32>().unwrap_or(0)).sum();
+
+    println!("{}", result)
+
+    // for n in &res {
+    //     println!("{}", n);
+    // }
+    //
 }
