1bd4ec51c8ecb53043cc1c2d518b7b327c0b05e2
Petr Kubelka <ksonny4@gmail.com>
2023-12-04 22:05:31 +0100

    day 4

--- 2023/day04a/src/main.rs
+++ 2023/day04a/src/main.rs
@@ -1,6 +1,29 @@
+use std::collections::HashSet;
+
 fn main() {
-    let data: Vec<Vec<char>> = include_str!("input.txt")
+    let data: Vec<Vec<HashSet<String>>> = include_str!("input.txt")
         .lines()
-        .map(|line| line.chars().collect())
+        .map(|s| {
+            s.split(':')
+                .nth(1)
+                .unwrap_or_default()
+                .split('|')
+                .map(|part| part.trim().split_whitespace().map(String::from).collect())
+                .collect()
+        })
         .collect();
+
+    let scores: Vec<i32> = data.iter().map(|sets| match sets.len() {
+        2 => sets[0].intersection(&sets[1]).count() as i32,
+        _ => 0,
+    }).collect();
+    println!("{:?}", scores);
+
+    let final_score: i32 = scores.iter().map(|&g| {
+        match g {
+            0 => 0,
+            _ => 2_i32.pow((g - 1) as u32),
+        }
+    }).sum();
+    println!("{:?}", final_score);
 }
--- 2023/day04b/src/main.rs
+++ 2023/day04b/src/main.rs
@@ -1,6 +1,55 @@
+
+use std::collections::{HashMap, HashSet, VecDeque};
+
+
+
 fn main() {
-    let data: Vec<Vec<char>> = include_str!("input.txt")
+    let data: Vec<Vec<HashSet<String>>> = include_str!("input.txt")
         .lines()
-        .map(|line| line.chars().collect())
+        .map(|s| {
+            s.split(':')
+                .nth(1)
+                .unwrap_or_default()
+                .split('|')
+                .map(|part| part.trim().split_whitespace().map(String::from).collect())
+                .collect()
+        })
         .collect();
+
+    println!("{:?}", data);
+
+    let wins: Vec<i32> = data.iter().map(|sets| match sets.len() {
+        2 => sets[0].intersection(&sets[1]).count() as i32,
+        _ => 0,
+    }).collect();
+    println!("Wins: {:?}", wins);
+
+
+    let mut stack: VecDeque<usize> = VecDeque::new();
+
+    let mut counter = 0; // for 1st instance of card
+
+    (0..wins.len()).into_iter().for_each(|x| {
+        stack.push_back(x);
+    });
+
+
+
+    while !stack.is_empty() {
+        counter += 1;
+        let index: usize = stack.pop_front().unwrap_or(9999999);
+        //println!("Card: {}, index: {}", index+1, index);
+
+        let score = wins[index];
+        let next_cards: Vec<usize> = (index + 1 + 1..score as usize + index + 2).collect();
+
+        next_cards.iter().for_each(|x| {
+            stack.push_back(*x-1);
+        });
+
+       // println!("Next cards from {} -> {:?}. Stack: {:?}. Counter: {:?}", index , next_cards, stack, counter);
+
+    }
+
+    println!("{}", counter);
 }
