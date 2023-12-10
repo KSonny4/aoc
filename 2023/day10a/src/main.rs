62140fdbdccb64ce83cf788853099db2bb22a20f
Petr Kubelka <ksonny4@gmail.com>
2023-12-10 21:03:58 +0100

    day 10

---
+++ 2023/day10a/src/main.rs
@@ -0,0 +1,270 @@
+use std::collections::{HashSet, VecDeque};
+
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
+struct Point {
+    value: String,
+    loc_x: usize,
+    loc_y: usize
+}
+
+
+fn dfs(start: &Point, input: Vec<Vec<Point>>) -> Option<Vec<Point>> {
+    let mut visited: HashSet<Point> = HashSet::new();
+    let mut history: Vec<Point> = Vec::new();
+    let mut stack: VecDeque<Point> = VecDeque::new();
+
+    stack.push_front(start.clone());
+    visited.insert(start.clone());
+
+    while let Some(current_vertex) = stack.pop_front() {
+
+        history.push(current_vertex.clone());
+
+        let current_vertex_conns = connected_to(current_vertex.clone(), input.clone());
+        for neighbor in current_vertex_conns {
+            let neigh_vertex_conns = connected_to(neighbor.clone(), input.clone());
+            if neigh_vertex_conns.contains(&current_vertex) {
+                if neighbor.value == "S" && history.len() > 2 {
+                    history.push(neighbor);
+                    return Some(history);
+                }
+
+                if visited.insert(neighbor.clone()) {
+                    stack.push_front(neighbor.clone());
+                    break; // no need for other vecs
+                }
+            }
+        }
+    }
+    None
+}
+
+fn main() {
+    let input: Vec<Vec<Point>> = include_str!("input.txt")
+        .lines()
+        .enumerate()
+        .map(|(index, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .map(|(index2, s)| Point {
+                    value: s.to_string(),
+                    loc_x: index,
+                    loc_y: index2,
+                })
+                .collect()
+        })
+        .collect();
+
+    // println!("{:?}", input);
+
+    // for i in &input {
+    //     let g: String = i.iter().map(|x| x.value.clone()).collect();
+    //     println!("{:#?}", g);
+    // }
+
+
+    let start: &Point = input.iter().find_map(| row| {
+        row.iter().find_map(|col| {
+            if col.value == "S" {
+                Some(col)
+            } else {
+                None
+            }
+        })
+    }).unwrap();
+
+
+    //let input_with_neigbors = fill_neighbors(input.clone());
+    //println!("{:?}", input_with_neigbors);
+
+    // println!("{:?}", start);
+
+
+
+    let cycle = dfs(start, input.clone());
+
+    // println!("{:#?}", cycle.clone());
+    println!("{:?}", cycle.clone().unwrap().len());
+    println!("{:?}", cycle.clone().unwrap().len() / 2);
+}
+
+
+fn connected_to(current_point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    // | is a vertical pipe connecting north and south.
+    // - is a horizontal pipe connecting east and west.
+    // L is a 90-degree bend connecting north and east.
+    // J is a 90-degree bend connecting north and west.
+    // 7 is a 90-degree bend connecting south and west.
+    // F is a 90-degree bend connecting south and east.
+    // . is ground; there is no pipe in this tile.
+    // S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+
+    match current_point.value.as_str() {
+        "|" => vertical_pipe_conns(&current_point, input),
+          "-" => horizontal_pipe_conns(current_point, input),
+         "L" => L_pipe_conns(current_point, input),
+         "J" => J_pipe_conns(current_point, input),
+         "7" => seven_pipe_conns(current_point, input),
+        "F" => F_pipe_conns(current_point, input),
+         "." => vec![],
+         "S" => S_pipe_conns(current_point, input),
+        _ => panic!()
+
+
+    }
+}
+
+fn vertical_pipe_conns(point: &Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for &col_offset in &[1, -1] {
+        let new_row_idx = point.loc_x as isize + col_offset;
+        let new_col_idx = point.loc_y as isize;
+
+        // Check if the new point is within bounds
+        if new_col_idx >= 0 && new_col_idx < input[0].len() as isize {
+            let conn = input[new_row_idx as usize][new_col_idx as usize].clone();
+            if conn.value != "." {
+                v.push(conn);
+            }
+        }
+    }
+    v
+}
+
+
+fn S_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let offsets = [
+         (-1, 0),
+        (0, -1), (0, 1),
+         (1, 0),
+    ];
+
+    let mut neighbors: Vec<Point> = Vec::new();
+
+    for &(row_offset, col_offset) in &offsets {
+        let new_row_idx = point.loc_x as isize + row_offset;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_row_idx >= 0
+            && new_row_idx < input.len() as isize
+            && new_col_idx >= 0
+            && new_col_idx < input[0].len() as isize
+        {
+            let neighbor = input[new_row_idx as usize][new_col_idx as usize].clone();
+
+            if neighbor.value != "." {
+                neighbors.push(neighbor);
+            }
+        }
+    }
+
+    neighbors
+}
+
+fn F_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for option in &[&[0, 1], &[1, 0]] {
+        let (row_offset, col_offset) = (option[0], option[1]);
+        let new_row_idx = point.loc_x as isize + row_offset;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_row_idx >= 0 && new_row_idx < input.len() as isize
+            && new_col_idx >= 0 && new_col_idx < input[0].len() as isize
+        {
+            let conn = &input[new_row_idx as usize][new_col_idx as usize];
+            if conn.value != "." {
+                v.push(conn.clone());
+            }
+        }
+    }
+    v
+}
+fn seven_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for option in &[&[0, -1], &[1, 0]] {
+        let (row_offset, col_offset) = (option[0], option[1]);
+        let new_row_idx = point.loc_x as isize + row_offset;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_row_idx >= 0 && new_row_idx < input.len() as isize
+            && new_col_idx >= 0 && new_col_idx < input[0].len() as isize
+        {
+            let conn = &input[new_row_idx as usize][new_col_idx as usize];
+            if conn.value != "." {
+                v.push(conn.clone());
+            }
+        }
+    }
+    v
+}
+
+fn J_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for option in &[&[-1, 0], &[0, -1]] {
+        let (row_offset, col_offset) = (option[0], option[1]);
+        let new_row_idx = point.loc_x as isize + row_offset;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_row_idx >= 0 && new_row_idx < input.len() as isize
+            && new_col_idx >= 0 && new_col_idx < input[0].len() as isize
+        {
+            let conn = &input[new_row_idx as usize][new_col_idx as usize];
+            if conn.value != "." {
+                v.push(conn.clone());
+            }
+        }
+    }
+
+    v
+}
+
+
+fn L_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for option in &[&[-1, 0], &[0, 1]] {
+        let (row_offset, col_offset) = (option[0], option[1]);
+        let new_row_idx = point.loc_x as isize + row_offset;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_row_idx >= 0 && new_row_idx < input.len() as isize
+            && new_col_idx >= 0 && new_col_idx < input[0].len() as isize
+        {
+            let conn = &input[new_row_idx as usize][new_col_idx as usize];
+            if conn.value != "." {
+                v.push(conn.clone());
+            }
+        }
+    }
+
+    v
+}
+
+
+fn horizontal_pipe_conns(point: Point, input: Vec<Vec<Point>>) -> Vec<Point> {
+    let mut v = Vec::new();
+
+    for &col_offset in &[1, -1] {
+        let new_row_idx = point.loc_x as isize;
+        let new_col_idx = point.loc_y as isize + col_offset;
+
+        // Check if the new point is within bounds
+        if new_col_idx >= 0 && new_col_idx < input[0].len() as isize {
+            let conn = input[new_row_idx as usize][new_col_idx as usize].clone();
+            if conn.value != "." {
+                v.push(conn);
+            }
+        }
+    }
+    v
+}
---
+++ 2023/day10b/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+
+
+    let input: Vec<Vec<i32>> = include_str!("input.txt")
+        .lines()
+        .map(|line| line.trim().split(" ").map(|s| s.parse::<i32>().unwrap_or(0)).collect()).collect();
+
+    println!("{:?}", input);
+
+
+    let mut games: Vec<Vec<Vec<i32>>> = Vec::new();
+    input.iter().for_each(|line| {
+        let mut rows: Vec<Vec<i32>> = Vec::new();
+        let mut row: Vec<i32> = line.clone();
+        row.reverse();
+        rows.push(row.clone());
+
+        while !row.iter().all(|x| x == &0) {
+            let mut new_row: Vec<i32> = Vec::new();
+            for index in 1..row.len() {
+                new_row.push(row[index] - row[index-1]);
+            }
+            row = new_row.clone();
+            rows.push(row.clone());
+        }
+        games.push(rows);
+    });
+
+    //println!("{:?}", games);
+
+    games.iter_mut().for_each(|game|
+        {
+            let mut last_inserted_val = 0;
+            for (index,row) in game.iter_mut().rev().enumerate() {
+                if index == 0 {
+                    last_inserted_val = 0; // not really needed
+                }
+
+                let value_to_add = row.last().unwrap() + last_inserted_val;
+                row.push(value_to_add);
+                last_inserted_val = value_to_add;
+
+            }
+        }
+
+    );
+
+    //println!("{:#?}", games);
+
+    let res: i32 = games.iter().map(|x| x.first().unwrap().last().unwrap()).sum();
+    println!("{:?}", res);
+
+    // game.iter_mut().for_each(|g| {
+    //     g.iter_mut().rev() {
+    //         row.push(0);
+    //     });
+    // });
+
+
+}
