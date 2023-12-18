2c32250c68ef6a51ca98ff49e661db4846ade4f6
Petr Kubelka <ksonny4@gmail.com>
2023-12-18 22:23:42 +0100

    day 18

--- 2023/day18b/src/main.rs
+++ 2023/day18b/src/main.rs
@@ -1,11 +1,9 @@
 use regex::Regex;
-use itertools::{Itertools};
 
 #[derive(Debug)]
 struct Action {
     direction: Direction,
     value: i64,
-    color: String,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -22,30 +20,34 @@ fn char_to_direction(c: char) -> Direction {
         'D' => Direction::Down,
         'L' => Direction::Left,
         'R' => Direction::Right,
+        '0' => Direction::Right,
+        '1' => Direction::Down,
+        '2' => Direction::Left,
+        '3' => Direction::Up,
         _ => panic!("Invalid direction character: {}", c),
     }
 }
 
-
-
 fn calculate(actions: Vec<Action>) {
-    let mut location:(i64, i64) = (0, 0);
+    let mut location: (i64, i64) = (0, 0);
     let mut sum1: i64 = 0;
-    let mut sum2 :i64 = 0;
-    let mut sum_dir :i64 = 0;
+    let mut sum2: i64 = 0;
+    let mut sum_dir: i64 = 0;
 
     for action in &actions {
         let mut new_location = location.clone();
 
         let direction = match action.direction {
-            Direction::Left => ( -1, 0),
-            Direction::Right => ( 1, 0),
-            Direction::Up => (0 , -1),
-            Direction::Down => (0 ,  1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
         };
 
-
-        new_location = (location.0 + direction.0 * action.value, location.1 + direction.1 * action.value);
+        new_location = (
+            location.0 + direction.0 * action.value,
+            location.1 + direction.1 * action.value,
+        );
         sum1 = sum1 + (location.0) * (new_location.1);
         sum2 = sum2 + (location.1) * (new_location.0);
         sum_dir += action.value;
@@ -56,17 +58,23 @@ fn calculate(actions: Vec<Action>) {
     println!("area: {:?}", area + sum_dir / 2 + 1);
 }
 
-
-
 fn main() {
     let input = include_str!("input.txt");
     let re = Regex::new(r"([RDLU]) (\d+) \((#[0-9a-fA-F]{6})\)").unwrap();
+    let hexa_to_int = |s: &str| i64::from_str_radix(s, 16).unwrap();
     let actions: Vec<Action> = re
         .captures_iter(input)
-        .map(|cap| Action {
-            direction: char_to_direction(cap[1].chars().next().unwrap()),
-            value: cap[2].parse().unwrap(),
-            color: cap[3].to_string(),
+        .map(|cap| {
+            let hex_color = &cap[3][1..6];
+            let value = hexa_to_int(hex_color);
+
+            let direction_char = &cap[3][6..7];
+            let direction = char_to_direction(direction_char.chars().next().unwrap());
+
+            Action {
+                direction,
+                value,
+            }
         })
         .collect();
 
